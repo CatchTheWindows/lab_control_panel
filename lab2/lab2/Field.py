c591b29f6c261ca950e129c6de39c90b638bcd91
CatchTheWindows <yamxty@gmail.com>
2018-03-20 20:47:01 +0200

    trying to add a branch

--- lab2/lab2/Field.py
+++ lab2/lab2/Field.py
@@ -4,6 +4,6 @@ import math
 def circular_field(angle):
 	angle = math.radians(angle)
 	a = 1
-	b = -a / (1/math.cos(angle)**2 -1)
+	b = -a / (1/((math.cos(angle)**2) -1))
 	return numpy.array([a,b])
 	
--- lab2/lab2/index.py
+++ lab2/lab2/index.py
@@ -1,29 +1,28 @@
 from server import handler,run,post
 from server import RequestHandler
+
 @handler("index")
 def printL(req):
-    data=""
-    if "?" in req.path:
-        getdict=req.parse_get()
-        if getdict["answer"] == "fail":
-            data = "<script>alert('This is not the correct password')</script>"
-        else:
-            data = "<script>alert('changing the parameters of the get request wont do something)</script>"
     with open('index.html', 'r') as myfile:
-        data+=myfile.read().replace('\n', '')
-    #return_code,headers,code of the page(here it is html)
-    return 200,{"Content-type": "text/html"},data
+        data=myfile.read().replace('\n', '')
+    return 200,{"Content-type":"text/html"}, data
 
-@handler("control","POST")
+@handler("af","POST")
 def answerpost(req):
-    data=str(post(req),"utf-8")
-    password = data.split("&")[0].split("=")[1]
-    if(password== "Skyro"):
-        with open('control_panel.html', 'r') as myfile:
-            data=myfile.read().replace('\n', '')
-        print(data[0])
-        return 200,{"Content-type": "text/html"},data
-    else:
-        return 302,{"Location": "/index?answer=fail"},data 
+        data1=str(post(req))
+        data1=data1[1:]
+        data="<head><title>Done</title>"
+        if data1!="'yourPassword=1234'":
+               data="<center><h1>You're wrong</h1><br/><a href='index'>Back</a></center>"
+        else:
+              with open('after.html', 'r') as myfile:
+                   data=myfile.read().replace('\n', '')
+        return 200,{"Content-type":"text/html"}, data
 
+
+@handler("abc","POST")
+def answerpost1(req):
+     with open('info.html', 'r') as myfile:
+             data=myfile.read().replace('\n', '')
+     return 200,{"Content-type":"text/html"}, data
 run()
